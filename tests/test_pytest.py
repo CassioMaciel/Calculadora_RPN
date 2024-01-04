10fb846e45d891f0fc3937afd19fb0dbdc816d9b
Cassio Maciel Lemos <cassio.lemos@petrobras.com.br>
2024-01-04 14:45:46 -0300

    somente resolvi os warnings

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,9 +1,4 @@
-import main
-import Interface_Homem_Maquina
 import operacoes as op
-import clear
-from pytest import mark
-
 
 
 def test_adiciona_10_ao_stack():
@@ -11,11 +6,9 @@ def test_adiciona_10_ao_stack():
            [10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 
-
 def test_adiciona_15_ao_stack():
     assert op.adiciona_ao_stack(15.0, [10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) == \
            [15.0, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
 
 
 def test_adiciona_20_ao_stack():
@@ -23,11 +16,9 @@ def test_adiciona_20_ao_stack():
            [20.0, 15.0, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 
-
 def test_adiciona_14_ao_stack():
     assert op.adiciona_ao_stack(14.0, [20.0, 15.0, 10.0, 13.0, 27.5, 12.0, 16.0, 14.0, 13.0, 10.0]) == \
            [14.0, 20.0, 15.0, 10.0, 13.0, 27.5, 12.0, 16.0, 14.0, 13.0]
-
 
 
 def test_soma_stack_1():
@@ -35,11 +26,9 @@ def test_soma_stack_1():
         [35.0, 10.0, 13.0, 27.5, 12.0, 16.0, 14.0, 13.0, 10.0, 0.0]
 
 
-
 def test_soma_stack_2():
     assert op.soma_stack([8.72, 15.21, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99]) == \
         [23.93, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99, 0.0]
-
 
 
 def test_subtrai_stack_1():
@@ -47,11 +36,9 @@ def test_subtrai_stack_1():
         [6.73, 13.76, 4.57, 15.93, 8.26, 10.09, 7.63, 14.28, 6.51, 0.0]
 
 
-
 def test_subtrai_stack_2():
     assert op.subtrai_stack([16.88, 5.32, 12.74, 8.15, 3.09, 19.44, 11.27, 6.78, 9.02, 14.37]) == \
         [-11.56, 12.74, 8.15, 3.09, 19.44, 11.27, 6.78, 9.02, 14.37, 0.0]
-
 
 
 def test_subtrai_stack_3():
@@ -59,11 +46,9 @@ def test_subtrai_stack_3():
         [6.49, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99, 0.0]
 
 
-
 def test_subtrai_stack_4():
     assert op.subtrai_stack([6.88, 14.26, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55]) == \
         [7.38, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55, 0.0]
-
 
 
 def test_multiplica_stack_1():
@@ -71,11 +56,9 @@ def test_multiplica_stack_1():
         [22.491, 13.76, 4.57, 15.93, 8.26, 10.09, 7.63, 14.28, 6.51, 0.0]
 
 
-
 def test_multiplica_stack_2():
     assert op.multiplica_stack([16.88, 5.32, 12.74, 8.15, 3.09, 19.44, 11.27, 6.78, 9.02, 14.37]) == \
         [89.8016, 12.74, 8.15, 3.09, 19.44, 11.27, 6.78, 9.02, 14.37, 0.0]
-
 
 
 def test_multiplica_stack_3():
@@ -83,11 +66,9 @@ def test_multiplica_stack_3():
         [132.6312, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99, 0.0]
 
 
-
 def test_multiplica_stack_4():
     assert op.multiplica_stack([6.88, 14.26, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55]) == \
         [98.1088, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55, 0.0]
-
 
 
 def test_divide_stack_1():
@@ -109,23 +90,22 @@ def test_divide_stack_4():
     assert op.divide_stack([6.88, 14.26, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55]) == \
         [2.072674418604651, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55, 0.0]
 
-def test_se_e_um_numero_recebe_3_e_retorna_que_é():
-    assert op.e_numero("3") == True
 
-def test_se_e_um_numero_recebe_207_e_retorna_que_é():
-    assert op.e_numero("2.07") == True
+def test_se_e_um_numero_recebe_3_e_retorna_que_e():
+    assert op.e_numero("3") is True
 
-def test_se_e_um_numero_recebe_207_e_retorna_que_é():
-    assert op.e_numero("2.07") == True
+
+def test_se_e_um_numero_recebe_207_e_retorna_que_e():
+    assert op.e_numero("2.07") is True
+
 
 def test_se_e_um_numero_recebe_joao_e_retorna_que_nao_e():
-    assert op.e_numero("João") == False
-
-def test_se_e_um_numero_recebe_joao_e_retorna_que_nao_e():
-    assert op.e_numero("+") == False
+    assert op.e_numero("João") is False
 
 
-#-------------------------------------------------------------------------------------------------------
+def test_se_e_um_numero_recebe_plus_e_retorna_que_nao_e():
+    assert op.e_numero("+") is False
+
 
 def test_calculadora_adiciona_10_ao_stack():
     assert op.calculadora(10.0, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) == \
@@ -137,11 +117,9 @@ def test_calculadora_adiciona_15_ao_stack():
            [15.0, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 
-
 def test_calculadora_adiciona_20_ao_stack():
     assert op.calculadora(20.0, [15.0, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) == \
            [20.0, 15.0, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
 
 
 def test_calculadora_adiciona_14_ao_stack():
@@ -149,11 +127,9 @@ def test_calculadora_adiciona_14_ao_stack():
            [14.0, 20.0, 15.0, 10.0, 13.0, 27.5, 12.0, 16.0, 14.0, 13.0]
 
 
-
 def test_calculadora_soma_20_e_15():
     assert op.calculadora('+', [20.0, 15.0, 10.0, 13.0, 27.5, 12.0, 16.0, 14.0, 13.0, 10.0]) == \
         [35.0, 10.0, 13.0, 27.5, 12.0, 16.0, 14.0, 13.0, 10.0, 0.0]
-
 
 
 def test_calculadora_soma_8_e_15():
@@ -161,11 +137,9 @@ def test_calculadora_soma_8_e_15():
         [23.93, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99, 0.0]
 
 
-
 def test_calculadora_subtrai_2_de_9():
     assert op.calculadora('-', [2.45, 9.18, 13.76, 4.57, 15.93, 8.26, 10.09, 7.63, 14.28, 6.51]) == \
         [6.73, 13.76, 4.57, 15.93, 8.26, 10.09, 7.63, 14.28, 6.51, 0.0]
-
 
 
 def test_calculadora_subtrai_stack_16_de_5():
@@ -173,11 +147,9 @@ def test_calculadora_subtrai_stack_16_de_5():
         [-11.56, 12.74, 8.15, 3.09, 19.44, 11.27, 6.78, 9.02, 14.37, 0.0]
 
 
-
 def test_calculadora_subtrai_8_de_15():
     assert op.calculadora('-', [8.72, 15.21, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99]) == \
         [6.49, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99, 0.0]
-
 
 
 def test_calculadora_subtrai_6_de_14():
@@ -185,11 +157,9 @@ def test_calculadora_subtrai_6_de_14():
         [7.38, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55, 0.0]
 
 
-
 def test_calculadora_multiplica_2_e_9():
     assert op.calculadora('*', [2.45, 9.18, 13.76, 4.57, 15.93, 8.26, 10.09, 7.63, 14.28, 6.51]) == \
         [22.491, 13.76, 4.57, 15.93, 8.26, 10.09, 7.63, 14.28, 6.51, 0.0]
-
 
 
 def test_calculadora_multiplica_16_e_5():
@@ -197,17 +167,14 @@ def test_calculadora_multiplica_16_e_5():
         [89.8016, 12.74, 8.15, 3.09, 19.44, 11.27, 6.78, 9.02, 14.37, 0.0]
 
 
-
 def test_calculadora_multiplica_8_e_15():
     assert op.calculadora('*', [8.72, 15.21, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99]) == \
         [132.6312, 3.45, 20.83, 7.11, 13.77, 9.04, 18.36, 14.5, 2.99, 0.0]
 
 
-
 def test_calculadora_multiplica_6_e_14():
     assert op.calculadora('*', [6.88, 14.26, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55]) == \
         [98.1088, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55, 0.0]
-
 
 
 def test_calculadora_divide_9_por_2():
@@ -228,6 +195,7 @@ def test_calculadora_divide_15_8():
 def test_calculadora_divide_14_por_6():
     assert op.calculadora('/', [6.88, 14.26, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55]) == \
         [2.072674418604651, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55, 0.0]
+
 
 def test_calculadora_string_qualquer():
     assert op.calculadora('joao', [6.88, 14.26, 10.93, 3.75, 7.42, 12.19, 19.85, 5.11, 18.04, 11.55]) == \
